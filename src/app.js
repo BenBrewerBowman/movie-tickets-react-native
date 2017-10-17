f79cb9eeb0e089da82841b4fbb5a65408ce5c42a
BenDaG <benbrewerbowman@gmail.com>
2017-10-16 22:09:44 -0600

    Redux storage available to all components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,49 @@
-import {StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { apiMiddleware, reducer } from './redux';
 import Movies from './Movies';
 import Confirmation from './Confirmation';
 
+
+// Create Redux store
+const store = createStore(reducer, {}, applyMiddleware(apiMiddleware));
+
+// Fetch movie data
+store.dispatch({type: 'GET_MOVIE_DATA'});
+
+// React view routing
 const RouteApp = StackNavigator({
     Movies: { screen : Movies},
     Confirmation:{ screen: Confirmation}
 });
 
-export default RouteApp;
+const RouteMapper = (route, navigator) => {
+  if (route.name === 'movies') {
+    return (
+      <Movies navigator={navigator} />
+    );
+  }
+  else if (route.name === 'confirmation') {
+    return (
+      <Confirmation code={route.code} navigator = {navigator} />
+    );
+  }
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store = {store}>
+        <Navigator
+          // Default to movies route
+          initialRoute = {{ name: 'movies' }}
+          // Use FlotFromBottom transition between screens
+          configureScene = {(route, routeStack) => Navigator.SceneConfigs.FloatFromBottom}
+          // Pass a route mapper function
+          renderScene = {RouteMapper}
+        />
+      </Provider>
+    );
+  }
+}
